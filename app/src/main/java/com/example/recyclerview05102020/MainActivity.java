ff894882d7a53fc244ca46e863d2ba5afc1d94a6
phatdroid94 <phatdroid94@gmail.com>
2020-11-11 20:33:12 +0700

    data

--- app/src/main/java/com/example/recyclerview05102020/MainActivity.java
+++ app/src/main/java/com/example/recyclerview05102020/MainActivity.java
@@ -21,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
         mRcvShop = findViewById(R.id.recyclerView);
         mShopList = new ArrayList<>();
 
-        mShopList.add(new Shop("7 - eleven : Cửa hàng tiện lợi",50 ,""));
+        mShopList.add(new Shop("7 - eleven : Cửa hàng tiện lợi",50 ,"" ,"Tối thiểu 20k",10,"Deal giảm tới 15k",R.drawable.hinh7eleven,true));
+        mShopList.add(new Shop("Big C",30 ,"" ,"Tối thiểu 20k",15,"Deal giảm tới 15k",R.drawable.hinhbigc,false));
+        mShopList.add(new Shop("Circle K",20 ,"" ,"Tối thiểu 10k",20,"Deal giảm tới 15k",R.drawable.hinhcirclek,true));
+        mShopList.add(new Shop("Cooky market",1 ,"198 Nguyễn Văn Nghi, P.7, Gò Vấp, TP. HCM" ,"Tối thiểu 20k",10,"Deal giảm tới 15k",R.drawable.hinhcookymarket,true));
+        mShopList.add(new Shop("Coop - food",15 ,"" ,"Tối thiểu 30k",10,"Deal giảm tới 15k",R.drawable.hinhcoop_food,false));
     }
 }
