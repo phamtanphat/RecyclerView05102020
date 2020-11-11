1f24fddc03a407c469ba9893f470af0e5a78747a
phatdroid94 <phatdroid94@gmail.com>
2020-11-11 20:10:05 +0700

    bind data

--- app/src/main/java/com/example/recyclerview05102020/ShopAdapter.java
+++ app/src/main/java/com/example/recyclerview05102020/ShopAdapter.java
@@ -1,14 +1,54 @@
 package com.example.recyclerview05102020;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ShopAdapter {
+import java.util.List;
 
+
+public class ShopAdapter extends RecyclerView.Adapter<ShopAdapter.ShopViewHolder>{
+
+    List<Shop> mShopList;
+
+    public ShopAdapter(List<Shop> mShopList) {
+        this.mShopList = mShopList;
+    }
+
+    @NonNull
+    @Override
+    public ShopViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View v = layoutInflater.inflate(R.layout.item_shop,parent,false);
+        return new ShopViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ShopViewHolder holder, int position) {
+        Shop shop = mShopList.get(position);
+        if (shop.getOnline()){
+            holder.mImgOnLine.setImageResource(R.drawable.ic_online);
+        }else{
+            holder.mImgOnLine.setImageResource(R.drawable.ic_offline);
+        }
+        holder.mImgBackground.setImageResource(shop.getImage());
+        holder.mTvName.setText(shop.getName());
+        holder.mTvCountShop.setText(shop.getCountShops() + " địa điểm");
+        holder.mTvSaleOff.setText(shop.getSaleOf());
+        holder.mTvPrice.setText("Giá " + shop.getPrice());
+        holder.mTvPromotion.setText(shop.getPromotion());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mShopList != null ? mShopList.size() : 0;
+    }
 
     class ShopViewHolder extends RecyclerView.ViewHolder{
         ImageView mImgOnLine,mImgBackground;
